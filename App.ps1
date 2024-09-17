3d14e1853f65083f284f114ec4eaeba433d9900a
Space Time <Zeus6_6@163.com>
2024-09-17 23:46:13 +0800

    1.1.1.1 更新

--- App.ps1
+++ App.ps1
@@ -13,7 +13,11 @@ Class App {
     hidden [string] GetHostPath() {
         [string] $hostPath = [string]::Empty
 
-        while (-not (Test-Path $hostPath)) { $hostPath = Read-Host "输入 Cealing-Host-List.json 文件保存目录路径" }
+        while (-not (Test-Path $hostPath)) {
+            $hostPath = (Read-Host "输入 Cealing-Host-List.json 文件保存目录路径 (默认脚本根目录)").Trim("""")
+
+            if ([string]::IsNullOrWhiteSpace($hostPath)) { $hostPath = $PSScriptRoot }
+        }
 
         return $hostPath
     }
@@ -33,21 +37,35 @@ Class App {
                 continue
             }
             elseif ($listRule.Contains("General List End")) {
-                Add-Content (Join-Path $hostPath "Cealing-Host-List.json") "]"
+                Add-Content (Join-Path $hostPath "Cealing-Host-List.json") "]" -NoNewline
 
                 return
             }
 
             if ($listRule -notmatch "^[!@\[/]" -and -not [string]::IsNullOrWhiteSpace($listRule)) {
-                [string] $listDomain = $listRule.Trim() -replace "^[|.]+", [string]::Empty -replace "^https?://", [string]::Empty -replace "/.*$", [string]::Empty
+                [string] $listDomain = $listRule.Trim() -replace "^[|.*]+", [string]::Empty -replace "^https?://", [string]::Empty -replace "/.*$", [string]::Empty
+                [int] $tryCount = 3
 
                 if ($listDomain -eq $lastListDomain) { continue }
                 else { $lastListDomain = $listDomain }
 
-                [array] $hostIpAnswer = (Invoke-RestMethod "https://ns.net.kg/dns-query?name=$listDomain").Answer
+                while ($true) {
+                    try {
+                        [array] $hostIpAnswer = (Invoke-RestMethod "https://ns.net.kg/dns-query?name=$listDomain").Answer
 
-                if ($hostIpAnswer) { Add-Content (Join-Path $hostPath "Cealing-Host-List.json") "`t[[""*$listDomain""],"""",""$($hostIpAnswer[-1].data)""]," }
-                else { Write-Host "$listDomain 解析失败" }
+                        if ($hostIpAnswer) { Add-Content (Join-Path $hostPath "Cealing-Host-List.json") "`t[[""*$listDomain""],"""",""$($hostIpAnswer[-1].data)""]," }
+                        else { Write-Host "$listDomain 解析失败" }
+
+                        break
+                    }
+                    catch {
+                        if (-not $tryCount--) {
+                            Write-Host "$listDomain 解析失败"
+
+                            break
+                        }
+                    }
+                }
             }
         }
     }
