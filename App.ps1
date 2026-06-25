8ed0cfa2ff58558524b8320f732a4459fe443f16
Space Time <Zeus6_6@163.com>
2026-06-26 01:05:05 +0800

    1.1.4.0 更新

--- App.ps1
+++ App.ps1
@@ -1,3 +1,5 @@
+using namespace System.Text
+
 Class App {
     [void] Main() {
         $this.Welcome()
@@ -6,14 +8,14 @@ Class App {
     }
 
     hidden [void] Welcome() {
-        Clear-Host
+        try { Clear-Host } catch { Write-Verbose $_ }
         Write-Host "Console HostGenerator 启动!" -ForegroundColor Red
     }
 
     hidden [string] GetHostPath() {
         [string] $hostPath = [string]::Empty
 
-        while (-not (Test-Path $hostPath -PathType Container)) {
+        while (-not (Test-Path -LiteralPath $hostPath -PathType Container)) {
             $hostPath = (Read-Host "输入 Cealing-Host-List.json 文件保存目录路径 (默认脚本根目录)").Trim("""")
 
             if ([string]::IsNullOrWhiteSpace($hostPath)) { $hostPath = $PSScriptRoot }
@@ -26,10 +28,10 @@ Class App {
         [bool] $isGeneralList = $false
         [string] $lastListDomain = [string]::Empty
 
-        foreach ($listRule in [Text.Encoding]::UTF8.GetString([Convert]::FromBase64String((Invoke-WebRequest "https://gitlab.com/gfwlist/gfwlist/raw/master/gfwlist.txt"))) -split "`n") {
+        foreach ($listRule in [Encoding]::UTF8.GetString([Convert]::FromBase64String((Invoke-WebRequest "https://gitlab.com/gfwlist/gfwlist/raw/master/gfwlist.txt"))) -split "`n") {
             if (-not $isGeneralList) {
                 if ($listRule.Contains("General List Start")) {
-                    Set-Content (Join-Path $hostPath "Cealing-Host-List.json") "["
+                    Set-Content -LiteralPath (Join-Path $hostPath "Cealing-Host-List.json") "["
 
                     $isGeneralList = $true
                 }
@@ -42,28 +44,28 @@ Class App {
                 return
             }
 
-            if ($listRule -notmatch "^[!@\[/]" -and -not [string]::IsNullOrWhiteSpace($listRule)) {
-                [string] $listDomain = $listRule.Trim() -replace "^[|.*]+", [string]::Empty -replace "^https?://", [string]::Empty -replace "/.*$", [string]::Empty
-                [int] $tryCount = 3
+            if ($listRule -match "^[!@\[/]" -or [string]::IsNullOrWhiteSpace($listRule)) { continue }
 
-                if ($listDomain -eq $lastListDomain) { continue }
-                else { $lastListDomain = $listDomain }
+            [string] $listDomain = $listRule.Trim() -replace "^[|.*]+" -replace "^https?://" -replace "/.*$"
+            [int] $tryCount = 3
 
-                while ($true) {
-                    try {
-                        [array] $hostIpAnswer = (Invoke-RestMethod "https://ns.net.kg/dns-query?name=$listDomain").Answer
+            if ($listDomain -eq $lastListDomain) { continue }
+            $lastListDomain = $listDomain
 
-                        if ($hostIpAnswer) { Add-Content (Join-Path $hostPath "Cealing-Host-List.json") "`t[[""*$listDomain""],"""",""$($hostIpAnswer[-1].data)""]," }
-                        else { Write-Host "$listDomain 解析失败" }
+            while ($true) {
+                try {
+                    [array] $hostIpAnswer = (Invoke-RestMethod "https://ns.net.kg/dns-query?name=$listDomain").Answer
+
+                    if ($hostIpAnswer) { Add-Content (Join-Path $hostPath "Cealing-Host-List.json") "`t[[""*$listDomain""],"""",""$($hostIpAnswer[-1].data)""]," }
+                    else { Write-Host "$listDomain 解析失败" }
+
+                    break
+                }
+                catch {
+                    if (-not $tryCount--) {
+                        Write-Host "$listDomain 解析失败"
 
                         break
-                    }
-                    catch {
-                        if (-not $tryCount--) {
-                            Write-Host "$listDomain 解析失败"
-
-                            break
-                        }
                     }
                 }
             }
